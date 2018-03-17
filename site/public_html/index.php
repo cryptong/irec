c0c57d3659725690b8dc2da3669e1e55704c4692
cryptong <clickrubcom@gmail.com>
2018-03-17 17:25:36 +0200

    update

---
+++ site/public_html/index.php
@@ -0,0 +1,465 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="images/favicon.png">
+	<title>I Recognized</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+
+	
+	<div class="page-inner">
+
+	<div id="head-top" style="position: absolute; width: 100%; top: 0; ">
+		<div class="gtco-top">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 col-xs-6">
+						<div id="gtco-logo"><a href="/">I Recognized <em>.</em></a></div>
+					</div>
+					<div class="col-md-6 col-xs-6 social-icons">
+						<ul class="gtco-social-top">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<nav class="gtco-nav sticky-banner" role="navigation">
+			<div class="gtco-container">
+				
+				<div class="row">
+					<div class="col-xs-12 text-center menu-1">
+                                                <ul id="mmenu">
+							<li class="active"><a href="/">Main</a></li>
+							<li><a href="#hitw">How it works</a></li>
+							<li><a href="#offch">OffChain</a></li>
+							<li><a href="#rmap">Roadmap</a></li>
+							<li><a href="/whitepaper.pdf" target="_blank">Whitepaper</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</nav>
+	</div>
+	
+	<header id="gtco-header" class="gtco-cover gtco-cover-md" role="banner" style="background-image: url(images/img_bg_1.jpg)" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="gtco-container">
+			<div class="row row-mt-15em">
+				<div class="col-md-12 mt-text text-center animate-box" data-animate-effect="fadeInUp">
+					<h1>Who goes ? <strong>I Recognized</strong></h1>	
+					<h2>Face Detection System. Security and Automatically open door.</h2>
+					<!--<div class="text-center"><a href="https://www.youtube.com/" class="btn btn-primary btn-lg popup-vimeo">Watch the video</a></div>-->
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div class="flex-section gtco-gray-bg" id="hitw">
+		<div class="col-1">
+			<div class="text">
+
+				<div class="row row-pb-sm">
+					<div class="col-md-12">
+					<h2>How it works</h2>
+					
+					<p>The person approaches the protected object. Our system recognizes the face. The data is checked in the list of allowed.
+					The level of access is determined. Doors open automatically. Welcome!</p>
+					<p>Recording of detailed statistics. Time of passage of the control. Amount of visits. Amount of failures. Daily statistics.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<ul class="check-li">
+							<li>Verification of personality</li>
+							<li>Determining the level of access</li>
+							<li>Automatic door opening</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<ul class="check-li">
+							<li>Amount of visits</li>
+							<li>Verification time visit of personality</li>
+							<li>Detailed statistics</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-2 flex-img" style="background-image: url(images/img_1.jpg);"></div>
+	</div>
+	
+	<div class="gtco-cover gtco-cover-sm" style="background-image: url(images/img_bg_4.jpg)"  data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="gtco-container text-center">
+			<div class="display-t">
+				<div class="display-tc">
+					<h1 class="animate-box">Machine learning</h1>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div class="flex-section reverse" id="offch">
+		<div class="col-1">
+			<div class="text">
+
+				<div class="row row-pb-sm">
+					<div class="col-md-12">
+						<h2>OffChain &amp; BlockChain</h2>
+						<p>Distributed storage of data.</p>
+						<p>The data are saved in the cloud storage to reduce the cost of transactions.</p>
+						<p>The control sum is saved in blockchain once a day for safe the storage and ability to restore of the history.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<ul class="check-li">
+							<li>The cost of the operation from 0.001$</li>
+							<li>Detailed statistic data</li>
+							<li>Effective management</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<ul class="check-li">
+							<li>Daily summary save in blockchain</li>
+							<li>Minimum transaction costs</li>
+							<li>High reliability</li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="col-2 flex-img" style="background-image: url(images/img_bg_2.jpg);"></div>
+	</div>
+
+	<div class="gtco-cover gtco-cover-sm" style="background-image: url(images/img_bg_3.jpg)" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="gtco-container text-center">
+			<div class="display-t">
+				<div class="display-tc">
+					<h1 class="animate-box">Quality Over Quantity</h1>
+				</div>	
+			</div>
+		</div>
+	</div>
+	
+            <div class="overflow-hid" id="rmap"> 
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+						<h2>Roadmap</h2>
+						<p>Way from idea to product</p>
+					</div>
+				</div>
+				<div class="row">
+
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<a href="#" class="gtco-card-item">
+                                                        <br>
+							<div class="gtco-text">
+								<h2>Idea</h2>
+								<p></p>
+								<p><span class="btn btn-primary">November 2017</span></p>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<a href="#" class="gtco-card-item">
+                                                        <br>
+							<div class="gtco-text">
+								<h2>Project and Strategy</h2>
+								<p></p>
+								<p><span class="btn btn-primary">January 2018</span></p>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<a href="#" class="gtco-card-item">
+                                                        <br>
+							<div class="gtco-text">
+								<h2>Pre-Sale</h2>
+								<p></p>
+								<p><span class="btn btn-primary">February-March 2018</span></p>
+							</div>
+						</a>
+					</div>
+
+
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<a href="#" class="gtco-card-item">
+                                                        <br>
+							<div class="gtco-text">
+								<h2>ICO and Branding</h2>
+								<p></p>
+								<p><span class="btn btn-primary">March-April 2018</span></p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<a href="#" class="gtco-card-item">
+                                                        <br>
+							<div class="gtco-text">
+								<h2>Development</h2>
+								<p></p>
+								<p><span class="btn btn-primary">June-August 2018</span></p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<a href="#" class="gtco-card-item">
+                                                        <br>
+							<div class="gtco-text">
+								<h2>Introduction</h2>
+								<p></p>
+								<p><span class="btn btn-primary">September 2018</span></p>
+							</div>
+						</a>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		
+		<div id="gtco-features">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
+						<h2>For whom</h2>
+						<p>Wherever you need to open the door</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4 col-sm-6">
+						<div class="feature-center animate-box" data-animate-effect="fadeIn">
+							<span class="icon">
+                                                            <img src="images/ico-1.png" alt="">
+							</span>
+							<h3>for Private</h3>
+							<p>Entrance, gate, door. Up to 1000 checks. Up to 3 devices</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="feature-center animate-box" data-animate-effect="fadeIn">
+							<span class="icon">
+                                                            <img src="images/ico-2.png" alt="">
+							</span>
+							<h3>for Mini-office</h3>
+							<p>Small business system. Up to 10000 checks. Up to 10 devices</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="feature-center animate-box" data-animate-effect="fadeIn">
+							<span class="icon">
+                                                            <img src="images/ico-3.png" alt="">
+							</span>
+							<h3>for Busines</h3>
+							<p>Unlimited checks, minimum cost. Up to 100 devices</p>
+						</div>
+					</div>
+					
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="gtco-cover gtco-cover-sm" style="background-image: url(images/img_bg_5.jpg)"  data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="gtco-container text-center">
+			<div class="display-t">
+				<div class="display-tc">
+					<h1>We have high quality services that you will surely love!</h1>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="gtco-counter" class="gtco-section">
+		<div class="gtco-container">
+
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
+					<h2>IREC ERC20 TOKEN</h2>
+					<p><a href="https://etherscan.io/token/0x7184B3B8D4Ba5C9E5FdD8211Ff7d61C7724414cE" target="_blank">etherscan.io</a></p>
+					<p>All issue tokens and their distribution.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-center">
+						<span class="counter js-counter" data-from="0" data-to="10000000" data-speed="5000" data-refresh-interval="5">1000000</span>
+						<span class="counter-label">Issue</span>
+
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-center">
+						<span class="counter js-counter" data-from="0" data-to="500000" data-speed="500" data-refresh-interval="50">50000</span>
+						<span class="counter-label">Pre-Sale</span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-center">
+						<span class="counter js-counter" data-from="0" data-to="8000000" data-speed="4000" data-refresh-interval="50">800000</span>
+						<span class="counter-label">ICO</span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-center">
+						<span class="counter js-counter" data-from="0" data-to="1500000" data-speed="3000" data-refresh-interval="50">150000</span>
+						<span class="counter-label">Developers</span>
+
+					</div>
+				</div>
+					
+			</div>
+                    
+                    
+		</div>
+	</div>
+
+	
+
+	<div id="gtco-subscribe">
+		<div class="gtco-container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+					<h2>Buy tokens</h2>
+                                        
+                                        <p>Pre-Sale</p>
+                                        <p>1 ETH = 10000 IREC<br>
+                                        CrowdSale Contract expires 03/25/2018<br>
+                                        <input type="text" class="form-control text-center" value="0xd15C3954E35b5aCB0b9ab40f5b26a430AfFfC5f8">
+                                        </p>
+                                        
+					<p>cryptocurrency-exchange</p>
+                                        <code>temporarily unavailable</code>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="gtco-footer" role="contentinfo">
+		<div class="gtco-container">
+
+			<div class="row copyright">
+				<div class="col-md-12">
+					<p class="pull-left">
+                                            <small class="block">&copy; 2017-2018 irecognized.pro</small> 
+					</p>
+					<p class="pull-right">
+						<ul class="gtco-social-icons pull-right">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/sticky.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106964589-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-106964589-4');
+</script>
+
+	</body>
+</html>
+
